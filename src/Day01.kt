dc45b44bd9d6f65747182f193160316353f3c4e5
Víctor J. García <vjgarciaw@gmail.com>
2022-12-02 00:22:33 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = -1
+        var currentCalories = 0
+
+        input.forEach { line ->
+            if (line.isEmpty()) {
+                if (currentCalories > maxCalories) {
+                    maxCalories = currentCalories
+                }
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+        if (currentCalories > maxCalories) {
+            maxCalories = currentCalories
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val maxHeap = PriorityQueue<Int>(Collections.reverseOrder())
+        var currentCalories = 0
+
+        input.forEach { line ->
+            if (line.isEmpty()) {
+                if (currentCalories > 0) {
+                    maxHeap.add(currentCalories)
+                }
+                currentCalories = 0
+            } else {
+                currentCalories += line.toInt()
+            }
+        }
+        if (currentCalories > 0) {
+            maxHeap.add(currentCalories)
+        }
+
+        return (1..3).sumOf { maxHeap.poll() ?: 0 }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
+    check(part2(testInput) == 45_000)
 
     val input = readInput("Day01")
     println(part1(input))
